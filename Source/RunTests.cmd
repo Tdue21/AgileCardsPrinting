cf15368f2427bf982ef21165f77574b889096ae6
Thomas Due <thomas@due-net.dk>
2017-04-17 00:25:52 +0200

    Corrected name of nunit console.

--- Source/RunTests.cmd
+++ Source/RunTests.cmd
@@ -1,3 +1,1 @@
-rem set nunit=c:\Development\Work\Department\Tools\NUnit\3.0.1\bin\nunit3-console.exe
-set nunit=nunit-console-x86 
-%nunit% .\PrintIssueCards.Tests\bin\Debug\PrintIssueCards.Tests.dll
+nunit3-console.exe .\PrintIssueCards.Tests\bin\Debug\PrintIssueCards.Tests.dll
