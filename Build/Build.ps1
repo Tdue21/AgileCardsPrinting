2ecbf82a2d51bb3f2d6d8dbe18b19fd567111308
Thomas Due <thomas@due-net.dk>
2017-08-22 12:31:35 +0200

    Some clean up

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -3,22 +3,22 @@ Import-Module -Name .\Invoke-MsBuild.psm1
 $openCover      ="..\Source\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
 $reportGenerator="..\Source\packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe" 
 $nunitConsole   ="..\Source\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" 
-$coverTarget    ="..\Source\PrintIssueCards.Tests\bin\Debug\PrintIssueCards.Tests.dll"
+$coverTarget    ="..\Source\AgileCardsPrinting.Tests\bin\Debug\AgileCardsPrinting.Tests.dll"
 
 "$(Get-Date -f o) Restoring nuget packages."
 
-& .\nuget.exe restore ..\Source\PrintIssueCards.sln
+& .\nuget.exe restore ..\Source\AgileCardsPrinting.sln
 
 "$(Get-Date -f o) Starting MSBuild."
 
-$result = Invoke-MsBuild -Path ..\Source\PrintIssueCards.sln -ShowBuildOutputInCurrentWindow -BuildLogDirectoryPath . -KeepBuildLogOnSuccessfulBuilds -BypassVisualStudioDeveloperCommandPrompt
+$result = Invoke-MsBuild -Path ..\Source\AgileCardsPrinting.sln -ShowBuildOutputInCurrentWindow -BuildLogDirectoryPath . -KeepBuildLogOnSuccessfulBuilds -BypassVisualStudioDeveloperCommandPrompt
 
 "$(Get-Date -f o) MSBuild finished."
 
 if($result.BuildSucceeded -eq $true) {
 
     "$(Get-Date -f o) Starting OpenCover."
-    & $openCover -register:user -returntargetcode "-filter:+[PrintIssueCards]* -[*Test]* -[PrintIssueCards]*Annotations.*" -target:$nunitConsole -targetargs:$coverTarget
+    & $openCover -register:user -returntargetcode "-filter:+[AgileCardsPrinting]* -[*Test]* -[AgileCardsPrinting]*Annotations.*" -target:$nunitConsole -targetargs:$coverTarget
     
     "$(Get-Date -f o) Starting ReportGenerator."
     & $reportGenerator "-reports:results.xml" "-targetdir:.\coverage" "-reporttypes:HtmlSummary"
--- Build/CoverTests.cmd
+++ Build/CoverTests.cmd
@@ -9,7 +9,7 @@ set cover=packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
 set report=packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe 
 rem set console=nunit3-console.exe 
 set console=packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe 
-set target=PrintIssueCards.Tests\bin\Debug\PrintIssueCards.Tests.dll
+set target=AgileCardsPrinting.Tests\bin\Debug\AgileCardsPrinting.Tests.dll
 
-%cover% -register:user -returntargetcode "-filter:+[PrintIssueCards]* -[*Test]* -[PrintIssueCards]*Annotations.*" "-target:%console%" "-targetargs:%target%"
+%cover% -register:user -returntargetcode "-filter:+[PrintIssueCards]* -[*Test]* -[AgileCardsPrinting.Tests]*Annotations.*" "-target:%console%" "-targetargs:%target%"
 %report% "-reports:results.xml" "-targetdir:.\coverage" "-reporttypes:HtmlSummary"
